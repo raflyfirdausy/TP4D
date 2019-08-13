ae83d1f58ce9ec2b4841d5dcde08181bc9394299
RAFLI FIRDAUSY <rafly.firdausy@gmail.com>
2019-08-14 02:20:34 +0700

    rapihin

---
+++ app/src/main/java/id/okvi/tp4d/Activity/kajari/KajariActionLanjutanActivity.java
@@ -0,0 +1,16 @@
+package id.okvi.tp4d.Activity.kajari;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import id.okvi.tp4d.R;
+
+public class KajariActionLanjutanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kajari_action_lanjutan);
+    }
+}
