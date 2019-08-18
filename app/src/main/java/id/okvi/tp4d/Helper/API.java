70a196ae1bb63108d3aa5d42772fc5c24acf1fdb
Rafli Firdausy Irawan <rafly.firdausy@gmail.com>
2019-08-18 16:06:16 +0700

    change host

--- app/src/main/java/id/okvi/tp4d/Helper/API.java
+++ app/src/main/java/id/okvi/tp4d/Helper/API.java
@@ -2,7 +2,7 @@ package id.okvi.tp4d.Helper;
 
 public class API {
 
-    public final static String HOST_ROOT = "http://192.168.43.106/tp4d/api/";
+    public final static String HOST_ROOT = "https://okvi.000webhostapp.com/api/";
 
     public final static String LOGIN_PEMOHON = HOST_ROOT + "login_pemohon.php";
     public final static String REGISTER_PEMOHON = HOST_ROOT + "register_pemohon.php";
